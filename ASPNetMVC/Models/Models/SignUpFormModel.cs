683e481750dbb3618b3abd430a89f07c2065dc47
Elias Vestermark <eliasvester96@gmail.com>
2024-02-22 14:49:20 +0100

    Sign Up completed

---
+++ ASPNetMVC/Models/Models/SignUpFormModel.cs
@@ -0,0 +1,38 @@
+﻿using ASPNetMVC.Helpers;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASPNetMVC.Models.Models;
+
+public class SignUpFormModel
+{
+    [Display(Name = "First Name", Prompt = "Enter your first name", Order = 0)]
+    [Required(ErrorMessage = "Invalid first name")]
+    public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Last Name", Prompt = "Enter your last name", Order = 1)]
+    [Required(ErrorMessage = "Invalid last name")]
+    public string LastName { get; set; } = null!;
+
+    [DataType(DataType.EmailAddress)]
+    [Required(ErrorMessage = "Invalid email")]
+    [Display(Name = "Email Address", Prompt = "Enter your email address", Order = 2)]
+    [RegularExpression("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email address")]
+    public string Email { get; set; } = null!;
+
+    [Display(Name = "Password", Prompt = "Enter your password", Order = 3)]
+    [Required(ErrorMessage = "Invalid password")]
+    [DataType(DataType.Password)]
+    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()\\-_=+{};:,<.>]).{8,}$", ErrorMessage = "Invalid password")]
+    public string Password { get; set; } = null!;
+
+    [Display(Name = "Confirm password", Prompt = "Confirm your password", Order = 4)]
+    [Required(ErrorMessage = "Password doesn't match")]
+    [DataType(DataType.Password)]
+    [Compare(nameof(Password), ErrorMessage = "Password doesn't match")]
+    public string ConfirmPassword { get; set; } = null!;
+
+    [Display(Name = "I aggre to the Terms & Conditions",  Order = 5)]
+    [CheckboxRequired(ErrorMessage = "You must accept the terms and conditions")]
+    public bool TermsAndCondition { get; set; } = false;
+}
--- ASPNetMVC/Models/Sections/SignUpViewModel.cs
+++ ASPNetMVC/Models/Sections/SignUpViewModel.cs
@@ -1,11 +1,18 @@
 ﻿using ASPNetMVC.Models.Components;
+using ASPNetMVC.Models.Models;
 
 namespace ASPNetMVC.Models.Sections;
 
 public class SignUpViewModel
 {
-    public string Id { get; set; } = null!;
-    public string Title { get; set; } = null!;
-    public string Text { get; set; } = null!;
-    public LinkViewModel Link { get; set; } = null!;
+    public string Id { get; set; } = "sign-up";
+    public string Title { get; set; } = "Create Account";
+    public string Text { get; set; } = "Already have an account?";
+    public LinkViewModel Link { get; set; } = new LinkViewModel
+    {
+        ControllerName = "Account",
+        ActionName = "SignIn",
+        Text = "Sign in here"
+    };
+    public SignUpFormModel Form { get; set; } = new SignUpFormModel();
 }
