5335f47bdc55892f56d59815e0cfbfba18243e67
Elias Vestermark <eliasvester96@gmail.com>
2024-02-23 18:54:42 +0100

    javascript validation done for sign up

--- ASPNetMVC/wwwroot/js/site.js
+++ ASPNetMVC/wwwroot/js/site.js
@@ -1,6 +1,7 @@
 ﻿const toggleMenu = () => {
     document.getElementById('menu').classList.toggle('hide')
     document.getElementById('buttons').classList.toggle('hide')
+    document.getElementById('button').classList.toggle('hide')
     document.getElementById('switch').classList.toggle('hide')
 }
 
@@ -8,6 +9,7 @@ const checkSize = () => {
     if (window.innerWidth >= 1200) {
         document.getElementById('menu').classList.remove('hide')
         document.getElementById('buttons').classList.remove('hide')
+        document.getElementById('button').classList.remove('hide')
         document.getElementById('switch').classList.remove('hide')
     } else {
         if (!document.getElementById('menu').classList.contains('hide')) {
@@ -18,6 +20,9 @@ const checkSize = () => {
         }
         if (!document.getElementById('switch').classList.contains('hide')) {
             document.getElementById('switch').classList.toggle('hide')
+        }
+        if (!document.getElementById('button').classList.contains('hide')) {
+            document.getElementById('button').classList.toggle('hide')
         }
     }
 }
