08777dbcf90f4721662713b82b5c787905104f50
Elias Vestermark <eliasvester96@gmail.com>
2024-02-23 10:06:57 +0100

    Added ProfileService constructor to profile controller

--- ASPNetMVC/Controllers/ProfileController.cs
+++ ASPNetMVC/Controllers/ProfileController.cs
@@ -4,5 +4,10 @@ namespace ASPNetMVC.Controllers;
 
 public class ProfileController : Controller
 {
-    
+    //private readonly ProfileService _profileService;
+
+    //public ProfileController(ProfileService profileService)
+    //{
+    //    _profileService = profileService;
+    //}
 }
