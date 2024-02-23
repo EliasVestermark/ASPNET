d10ea190f991181e75772bc6418522aadab88697
Elias Vestermark <eliasvester96@gmail.com>
2024-02-23 08:09:56 +0100

    Sign in done

--- ASPNetMVC/Models/Views/SignInViewModel.cs
+++ ASPNetMVC/Models/Views/SignInViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace ASPNetMVC.Models.Views
+﻿using ASPNetMVC.Models.Components;
+using ASPNetMVC.Models.Models;
+
+namespace ASPNetMVC.Models.Views
 {
     public class SignInViewModel
     {
+        public string Id { get; set; } = "sign-in";
+        public string Title { get; set; } = "Welcome Back!";
+        public string Text { get; set; } = "Don't have an account yet?";
+        public LinkViewModel Link { get; set; } = new LinkViewModel
+        {
+            ControllerName = "Account",
+            ActionName = "SignUp",
+            Text = "Sign up here"
+        };
+        public SignInFormModel Form { get; set; } = new SignInFormModel();
     }
 }
